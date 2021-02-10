f448f18d92c6ab7533296206a35c0295b8fdfa84
Dawid Dębowski <dawid199960@gmail.com>
2021-02-10 21:24:44 +0100

    Info page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS
 
 from database.Database import Database
@@ -13,7 +13,7 @@ CORS(app)
 
 @app.route("/")
 def hello_world():
-    return "hello world"
+    return render_template("index.html")
 
 
 def get_and_check_note_params(request):
@@ -28,42 +28,36 @@ def get_and_check_note_params(request):
 
 @app.route("/create_note", methods=["POST"])
 def create_note():
-    if request.method == "POST":
-        try:
-            title, content = get_and_check_note_params(request)
-        except WebException as e:
-            return e.message, e.code
-        database.create_note(title, content)
-        return "created", 201
-    return "Hellow get"
+    try:
+        title, content = get_and_check_note_params(request)
+    except WebException as e:
+        return e.message, e.code
+    database.create_note(title, content)
+    return "created", 201
 
 
 @app.route("/update_note/<note_id>", methods=["POST"])
 def update_note(note_id):
-    if request.method == "POST":
-        try:
-            title, content = get_and_check_note_params(request)
-        except WebException as e:
-            return e.message, e.code
-        try:
-            database.update_note_by_id(note_id, title, content)
-        except NoteNotFound:
-            return "There's no such note", 404
-        except NoteDeleted:
-            return "This note was already deleted, you can't update it", 410
-        return "Note was updated", 200
-    return "Hellow update"
+    try:
+        title, content = get_and_check_note_params(request)
+    except WebException as e:
+        return e.message, e.code
+    try:
+        database.update_note_by_id(note_id, title, content)
+    except NoteNotFound:
+        return "There's no such note", 404
+    except NoteDeleted:
+        return "This note was already deleted, you can't update it", 410
+    return "Note was updated", 200
 
 
 @app.route("/delete_note/<note_id>", methods=["POST"])
 def delete_note(note_id):
-    if request.method == "POST":
-        try:
-            database.delete_note_by_id(note_id)
-        except NoteNotFound:
-            return "There's no such note", 404
-        return "Note deleted", 200
-    return "Hello delete"
+    try:
+        database.delete_note_by_id(note_id)
+    except NoteNotFound:
+        return "There's no such note", 404
+    return "Note deleted", 200
 
 
 @app.route("/get_note/<note_id>", methods=["GET"])
